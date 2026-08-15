ca19f1a11d7ea9e64e0f9f9edea1144e4ee0421a
Your Name <153814332+Rishi-Mishima@users.noreply.github.com>
2026-08-15 04:30:50 +0200

    feat: initial project commit

---
+++ src/main/java/com/housingbid/HosingBidApplication.java
@@ -0,0 +1,13 @@
+package com.housingbid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HosingBidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HosingBidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/housingbid/HosingBidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.housingbid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HosingBidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
